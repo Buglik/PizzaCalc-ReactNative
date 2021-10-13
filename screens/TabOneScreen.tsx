f2abf61404a2c492d6d62180d97da9c45613581c
Radosław Łodziński <radeklodzinski@gmail.com>
2021-10-13 19:36:18 +0200

    Main screen POC

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,33 +1,153 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  StyleSheet,
+  View,
+  Switch,
+  Text,
+  TouchableWithoutFeedback,
+  Keyboard,
+  SafeAreaView,
+} from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import { Button, ToggleButton, TextInput } from 'react-native-paper';
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+const DismissKeyboard = ({ children }: { children: any }) => (
+  <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+    {children}
+  </TouchableWithoutFeedback>
+);
+
+export default function TabOneScreen({
+  navigation,
+}: RootTabScreenProps<'TabOne'>) {
+  const [price, setPrice] = useState('');
+
+  const [pizzaShape, setPizzaShape] = useState('circle');
+
+  const [diameter, setDiameter] = useState('');
+  const [circleResult, setCircleResult] = useState('');
+
+  const [length, setLength] = useState('');
+  const [width, setWidth] = useState('');
+  const [rectangleResult, setRectangleResult] = useState('');
+
+  const calcCircleResult = () => {
+    return (
+      ((parseInt(diameter) / 2) * (parseInt(diameter) / 2) * 3.14) /
+      parseInt(price)
+    )
+      .toFixed(0)
+      .toString();
+  };
+
+  const calcRectangleResult = () => {
+    return ((parseInt(width) * parseInt(length)) / parseInt(price))
+      .toFixed(0)
+      .toString();
+  };
+
+  useEffect(() => {
+    if (price && diameter) {
+      const result = calcCircleResult();
+      if (result !== 'NaN') setCircleResult(result);
+      else setCircleResult('Wrong input');
+    }
+
+    if (price && width && length) {
+      const result = calcRectangleResult();
+      if (result !== 'NaN') setRectangleResult(result);
+      else setRectangleResult('Wrong input');
+    }
+  }, [price, diameter, width, length]);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
+    <DismissKeyboard>
+      <SafeAreaView style={styles.container}>
+        <TextInput
+          onChangeText={setPrice}
+          value={price}
+          label="Price (zł)"
+          style={styles.input}
+          keyboardType={'numeric'}
+        />
+        <Text style={styles.label}>
+          {pizzaShape === 'circle' ? 'Circle' : 'Rectangle'}
+        </Text>
+        <ToggleButton.Row
+          onValueChange={(value) => setPizzaShape(value)}
+          value={pizzaShape}
+          style={{ margin: 10 }}
+        >
+          <ToggleButton icon="circle" value="circle" />
+          <ToggleButton icon="rectangle" value="rectangle" />
+        </ToggleButton.Row>
+
+        {pizzaShape === 'circle' ? (
+          <View style={{ margin: 10 }}>
+            <TextInput
+              onChangeText={setDiameter}
+              value={diameter}
+              label="Diameter (cm)"
+              style={styles.inputCircle}
+              keyboardType={'numeric'}
+            />
+          </View>
+        ) : (
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              margin: 10,
+            }}
+          >
+            <TextInput
+              onChangeText={setLength}
+              value={length}
+              label="Length (cm)"
+              style={styles.inputRectangle}
+              keyboardType={'numeric'}
+            />
+            <TextInput
+              onChangeText={setWidth}
+              value={width}
+              label="Width (cm)"
+              style={styles.inputRectangle}
+              keyboardType={'numeric'}
+            />
+          </View>
+        )}
+        {pizzaShape === 'circle' ? (
+          <Text style={styles.result}>Result: {circleResult}</Text>
+        ) : (
+          <Text style={styles.result}>Result: {rectangleResult}</Text>
+        )}
+      </SafeAreaView>
+    </DismissKeyboard>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    // display: 'flex',
+    // alignItems: 'center',
+    // justifyContent: 'space-between',
+    // height: 300,
+    margin: 20,
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  label: {
+    margin: 10,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  input: { margin: 10 },
+  inputCircle: {},
+  inputRectangle: {
+    // height: 40,
+    // fontSize: 15,
+    width: 150,
+  },
+  result: {
+    fontSize: 30,
+    margin: 10,
   },
 });
